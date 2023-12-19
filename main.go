31fba7e5cc99469e71fb6822c804b3a3adc5450a
wipdev-tech <wipdev.tech@gmail.com>
2023-12-19 20:37:56 +0200

    Recreating the stats graph on AoC's site
    The code needs some refactoring, before or after adding new features.

--- main.go
+++ main.go
@@ -4,32 +4,44 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"regexp"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
-	flgStat := flag.String("s", "total", "Total participants in the year.")
+	flgStat := flag.String("s", "graph", "Total participants in the year.")
 	flgYear := flag.Int("y", 2023, "Total participants in the year.")
 	flag.Parse()
 
 	switch *flgStat {
-	case "total":
-		handleTotal(*flgYear)
+	case "graph":
+		handleGraph(*flgYear)
 	}
 }
 
-func handleTotal(year int) {
+func handleGraph(year int) {
 	c := colly.NewCollector()
+
 	c.OnHTML(
-		"pre.stats span.stats-both, pre.stats span.stats-firstonly",
+		"a",
 		func(e *colly.HTMLElement) {
-			fmt.Println(e.Text)
+			href := e.Attr("href")
+			pattern := fmt.Sprintf("/%v/day/", year)
+
+			if strings.HasPrefix(href, pattern) {
+				firstOnlyStars := e.ChildText(".stats-firstonly:last-child")
+                firstOnlyStarsPattern := strings.Replace(firstOnlyStars, "*", "\\*", -1)
+				re := regexp.MustCompile(firstOnlyStarsPattern + "$")
+				fmt.Println(re.ReplaceAllString(e.Text, "|"+firstOnlyStars))
+			}
 		},
 	)
 
 	url := fmt.Sprintf("https://adventofcode.com/%v/stats", year)
 	fmt.Println("Visiting URL:", url)
+	fmt.Println("")
 
 	err := c.Visit(url)
 	if err != nil {
