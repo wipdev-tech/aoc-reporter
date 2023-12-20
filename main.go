ed42a7ad5317a74e2fcace4a16ae97ca136197eb
wipdev-tech <wipdev.tech@gmail.com>
2023-12-20 13:19:40 +0200

    Added total submissions in a given year

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/gocolly/colly"
@@ -18,6 +19,8 @@ func main() {
 	switch *flgStat {
 	case "graph":
 		handleGraph(*flgYear)
+	case "total":
+		handleTotal(*flgYear)
 	}
 }
 
@@ -32,7 +35,7 @@ func handleGraph(year int) {
 
 			if strings.HasPrefix(href, pattern) {
 				firstOnlyStars := e.ChildText(".stats-firstonly:last-child")
-                firstOnlyStarsPattern := strings.Replace(firstOnlyStars, "*", "\\*", -1)
+				firstOnlyStarsPattern := strings.Replace(firstOnlyStars, "*", "\\*", -1)
 				re := regexp.MustCompile(firstOnlyStarsPattern + "$")
 				fmt.Println(re.ReplaceAllString(e.Text, "|"+firstOnlyStars))
 			}
@@ -47,4 +50,32 @@ func handleGraph(year int) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handleTotal(year int) {
+	total := 0
+	c := colly.NewCollector()
+
+	c.OnHTML(
+		".stats-both:nth-child(1), .stats-firstonly:nth-child(2)",
+		func(e *colly.HTMLElement) {
+			totalInt, err := strconv.Atoi(strings.TrimSpace(e.Text))
+			if err != nil {
+				return
+			}
+			total += totalInt
+		},
+	)
+
+	url := fmt.Sprintf("https://adventofcode.com/%v/stats", year)
+	fmt.Println("Visiting URL:", url)
+	fmt.Println("")
+
+	err := c.Visit(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Total Submissions for %v:\n", year)
+	fmt.Println(total)
 }
